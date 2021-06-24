e67cb5f2c8b39c8017ae6117759741a79d0dad6f
yones-je <yones.je@gmail.com>
2021-06-24 12:33:03 +0200

    project structure set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Clock,
+  Footer,
+  Header,
+  ParticipantList,
+  SetParticipants,
+  SetTimer
+} from './components'
+import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <div className="input-container" >
+       <SetParticipants /> 
+       <SetTimer />
+     </div>
+     <Clock />
+     <ParticipantList />
+     <Footer />
     </div>
   );
 }
